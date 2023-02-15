0a00ddcb313527cc48fb83e9508ef335169f2c31
George Reeve <george.fr.reeve@gmail.com>
2023-02-15 10:56:53 +0000

    Use CONNECTION_STRING in environment variables

--- src/server.js
+++ src/server.js
@@ -1,9 +1,16 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
-try {
-	await mongoose.connect("mongodb://127.0.0.1:27017/backend-module-project");
-	// use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
-	console.log("CONNECTED");
-} catch (error) {
-	console.log(error);
-}
+async function connect() {
+    try {
+        await mongoose.connect(process.env.CONNECTION_STRING);
+        console.log("CONNECTED");
+    } catch (error) {
+        console.log(error);
+    }
+
+    // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+  }
+
+(async () => await connect())();
