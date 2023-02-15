13d0bff1ab07018095c8bb83095cfcb687cffaa5
Leon Semmens <114012182+leonsemmens-sky@users.noreply.github.com>
2023-02-15 10:52:09 +0000

    Update to ES6

--- src/server.js
+++ src/server.js
@@ -1,14 +1,9 @@
-const mongoose = require('mongoose');
+import mongoose from "mongoose";
 
-async function connect() {
-    try {
-        await mongoose.connect('mongodb://127.0.0.1:27017/backend-module-project');
-        console.log("CONNECTED");
-    } catch (error) {
-        console.log(error);
-    }
-
-    // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
-  }
-
-(async () => await connect())();
+try {
+	await mongoose.connect("mongodb://127.0.0.1:27017/backend-module-project");
+	// use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+	console.log("CONNECTED");
+} catch (error) {
+	console.log(error);
+}
