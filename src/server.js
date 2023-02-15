8b31acd557510db1da4127ed46263185dd9e1e71
George Reeve <george.fr.reeve@gmail.com>
2023-02-15 10:52:03 +0000

    Wrap in async

--- src/server.js
+++ src/server.js
@@ -11,4 +11,4 @@ async function connect() {
     // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
   }
 
-await connect();
+(async () => await connect())();
