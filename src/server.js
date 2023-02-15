dbb2fc197392fe0be31a1dd2bbe194bd23614bc0
George Reeve <george.fr.reeve@gmail.com>
2023-02-15 10:58:13 +0000

    Clean up formatting

--- src/server.js
+++ src/server.js
@@ -9,8 +9,6 @@ async function connect() {
     } catch (error) {
         console.log(error);
     }
-
-    // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
-  }
+}
 
 (async () => await connect())();
