8596def20366e5d5b99221ab4e83b18d6bde0b92
Teddy Putus <99917582+TeddyPutus@users.noreply.github.com>
2023-02-15 11:03:56 +0000

    merged serve js, now using import

--- src/server.js
+++ src/server.js
@@ -1,7 +1,13 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
+import express from 'express';
+import cors from 'cors';
 import dotenv from "dotenv";
 dotenv.config();
 
+const app = express(); //create a new instance of express
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cors()); //This allows post requests etc.
 async function connect() {
     try {
         mongoose.set('strictQuery', false); // The default in Mongoose 7
