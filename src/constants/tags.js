ca9c189cf45af5451c6fd49ae3e486ba4bee081c
Leon Semmens <114012182+leonsemmens-sky@users.noreply.github.com>
2023-02-17 12:44:33 +0000

    Add seeding

--- src/constants/tags.js
+++
@@ -1,3 +0,0 @@
-const TAGS = ["TAG"];
-
-export default TAGS;
---
+++ src/models/index.js
@@ -0,0 +1,5 @@
+import Rating from "./Rating.model.js";
+import Tune from "./Tune.model.js";
+import User from "./User.model.js";
+
+export { Rating, Tune, User };
