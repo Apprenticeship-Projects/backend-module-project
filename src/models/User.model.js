93c0482dcef737b3cb46d79586ab422050df2a99
Leon Semmens <114012182+leonsemmens-sky@users.noreply.github.com>
2023-02-17 10:17:08 +0000

    Make user model

--- src/models/User.model.js
+++ src/models/User.model.js
@@ -1,27 +1,89 @@
 import mongoose from "mongoose";
 
-const schema = new mongoose.Schema({
-	username: {
-		type: String,
-		trim: true,
-		minLength: 2,
-		maxLength: 32,
-		required: true,
-		unique: true,
+const schema = new mongoose.Schema(
+	{
+		username: {
+			type: String,
+			trim: true,
+			minLength: 2,
+			maxLength: 32,
+			required: true,
+			unique: true,
+		},
+		email: {
+			type: String,
+			trim: true,
+			minLength: 5,
+			required: true,
+			unique: true,
+		},
+		firstName: {
+			type: String,
+			trim: true,
+			minLength: 1,
+			required: true,
+		},
+		lastName: {
+			type: String,
+			trim: true,
+			minLength: 1,
+			required: true,
+			alias: "surname",
+		},
+		password: {
+			type: String,
+			minLength: 8,
+			maxLength: 20,
+			required: true,
+			validate: {
+				validator: (password) => {
+					return /^\$2[ayb]?\$[0-9]{2}\$[A-Za-z0-9\.\/]/.test(
+						password
+					);
+				},
+				message: () => `Password must be a hash!`,
+			},
+		},
+		sessions: {
+			type: [String],
+			default: [],
+		},
+		dob: {
+			type: Date,
+			required: true,
+			validate: {
+				validator: (date) => {
+					const maxDate = new Date();
+					maxDate.setFullYear(maxDate.getFullYear() - 13);
+					return date <= maxDate;
+				},
+				message: (props) =>
+					`DoB (${props.value}) is less than 13 years.`,
+			},
+		},
+		ratings: {
+			type: [
+				{
+					type: mongoose.Schema.Types.ObjectId,
+					ref: "Rating",
+				},
+			],
+			default: [],
+		},
+		tunes: {
+			type: [
+				{
+					type: mongoose.Schema.Types.ObjectId,
+					ref: "Tune",
+				},
+			],
+			default: [],
+		},
 	},
-	email: {
-		type: String,
-		trim: true,
-		minLength: 5,
-		required: true,
-		unique: true,
-	},
-	password: {
-		type: String,
-		minLength: 8,
-		maxLength: 20,
-	},
-});
+	{
+		timestamps: true,
+	}
+);
 
 const User = mongoose.model("User", schema);
 
