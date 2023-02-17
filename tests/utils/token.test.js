1d874b0fcd18e2f3b9be22d7c6d616615732182e
George Reeve <george.fr.reeve@gmail.com>
2023-02-17 10:29:16 +0000

    Format

--- tests/utils/token.test.js
+++ tests/utils/token.test.js
@@ -7,6 +7,7 @@ test("sign() returns a string", () => {
 test("verify() returns an object", () => {
     const token = sign("test");
     const obj = verify(token);
+
     expect(Object.keys(obj).length).toBe(3);
     expect(obj.id).toBe("test");
     expect(obj.iat.toString()).toMatch(/[0-9]{10,}/);
