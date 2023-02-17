8289c763e84c72d8d103250b3b1c265a5881926b
Leon Semmens <114012182+leonsemmens-sky@users.noreply.github.com>
2023-02-17 09:22:44 +0000

    Started user model

--- src/models/User.model.js
+++ src/models/User.model.js
@@ -1,6 +1,27 @@
 import mongoose from "mongoose";
 
-const schema = new mongoose.Schema({});
+const schema = new mongoose.Schema({
+	username: {
+		type: String,
+		trim: true,
+		minLength: 2,
+		maxLength: 32,
+		required: true,
+		unique: true,
+	},
+	email: {
+		type: String,
+		trim: true,
+		minLength: 5,
+		required: true,
+		unique: true,
+	},
+	password: {
+		type: String,
+		minLength: 8,
+		maxLength: 20,
+	},
+});
 
 const User = mongoose.model("User", schema);
 
