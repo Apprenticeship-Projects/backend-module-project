08bcf7632031ed476c8cb5c91881ac957f1464a5
Teddy Putus <99917582+TeddyPutus@users.noreply.github.com>
2023-02-17 09:50:14 +0000

    fixed merge conflicts

--- src/server.js
+++ src/server.js
@@ -3,35 +3,21 @@ dotenv.config();
 import mongoose from 'mongoose';
 import express from 'express';
 import cors from 'cors';
-<<<<<<< HEAD
-=======
-import dotenv from "dotenv";
-// import {userRouter, tuneRouter, sessionRouter} from "./routes";
-
-dotenv.config();
->>>>>>> e36e7e0 (set up express server, listening, connected routers)
 
 const app = express(); //create a new instance of express
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors()); //This allows post requests etc.
-<<<<<<< HEAD
 
-mongoose.set("strictQuery", false); // The default in Mongoose 7
-
-// If it errors we want the app to fail?
-await mongoose.connect(process.env.CONNECTION_STRING);
-=======
-
-// app.use('/users', userRouter);
-// app.use('/tunes', tuneRouter);
-// app.use('/session', sessionRouter);
+app.use('/users', userRouter);
+app.use('/tunes', tuneRouter);
+app.use('/session', sessionRouter);
 
 async function connect() {
     try {
         mongoose.set('strictQuery', false); // The default in Mongoose 7
         await mongoose.connect(process.env.CONNECTION_STRING);
-        console.log("CONNECTED");
+        console.log("Connected to MongoDB");
     } catch (error) {
         console.log(error);
     }
@@ -44,4 +30,3 @@ app.listen(process.env.PORT, async() => {
 });
 
 
->>>>>>> e36e7e0 (set up express server, listening, connected routers)
