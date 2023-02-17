adff1cfe3d91501501360ba4b818d354393d72a3
Leon Semmens <114012182+leonsemmens-sky@users.noreply.github.com>
2023-02-17 13:00:37 +0000

    format

--- src/routes/register.js
+++ src/routes/register.js
@@ -1,45 +1,46 @@
 import { Router } from "express";
-import {body, validationResult} from 'express-validator';
+import { body, validationResult } from "express-validator";
 import { checkErrors } from "../utils/validationMiddleware";
 import User from "../models/User.model.js";
 import { createHash } from "../utils/hash";
 
 const router = Router();
-const SALT_ROUNDS = 10;
+
 // Routes for /register
 
 //Register user
-router.post("/", 
-  body('email').notEmpty().isEmail(),
-  body('password').notEmpty().isLength({min: 8, max: 20}),
-  body('username').notEmpty(),
-  body('firstName').notEmpty(),
-  body('lastName').notEmpty(),
-  body('dob').notEmpty(),
-  checkErrors,
-  async (req, res) => {
-    try {
-      const hashedPass = await createHash(req.body.password);
+router.post(
+	"/",
+	body("email").notEmpty().isEmail(),
+	body("password").notEmpty().isLength({ min: 8, max: 20 }),
+	body("username").notEmpty(),
+	body("firstName").notEmpty(),
+	body("lastName").notEmpty(),
+	body("dob").notEmpty(),
+	checkErrors,
+	async (req, res) => {
+		try {
+			const hashedPass = await createHash(req.body.password);
 
-      const createdUser = new User({
-        username: req.body.username,
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        password: hashedPass,
-        email: req.body.email,
-        dob: Date.parse(req.body.dob)
-      });
+			const createdUser = new User({
+				username: req.body.username,
+				firstName: req.body.firstName,
+				lastName: req.body.lastName,
+				password: hashedPass,
+				email: req.body.email,
+				dob: Date.parse(req.body.dob),
+			});
 
-      console.log(createdUser)
+			console.log(createdUser);
 
-      await createdUser.save();
+			await createdUser.save();
 
-      res.status(200).send("registered");
-    } catch (error) {
-      console.log(error);
-      res.status(400).send(error);
-    }
-    
-});
+			res.status(200).send("registered");
+		} catch (error) {
+			console.log(error);
+			res.status(400).send(error);
+		}
+	}
+);
 
 export { router };
