0dfda94de9dbcbe9cc40cbc7019f80080fb62b5b
Leon Semmens <114012182+leonsemmens-sky@users.noreply.github.com>
2023-02-17 13:00:09 +0000

    Separate seed function

--- src/data/seed.js
+++ src/data/seed.js
@@ -1,28 +1,3 @@
-import bcrypt from "bcrypt";
-import mongoose from "mongoose";
-import dotenv from "dotenv";
-dotenv.config();
-import users from "./users.json" assert { type: "json" };
-import tunes from "./tunes.json" assert { type: "json" };
-import { User, Tune } from "../models/index.js";
-
-const SALT_ROUNDS = 10;
-
-mongoose.set("strictQuery", false); // The default in Mongoose 7
-await mongoose.connect(process.env.CONNECTION_STRING);
-
-async function seed() {
-	await User.deleteMany({});
-	await Tune.deleteMany({});
-
-	for (let user of users) {
-		user.password = await bcrypt.hash(user.password, SALT_ROUNDS);
-	}
-
-	await User.insertMany(users);
-	await Tune.insertMany(tunes);
-
-	await mongoose.disconnect();
-}
+import seed from "./seedFn";
 
 seed();
---
+++ src/data/seedFn.js
@@ -0,0 +1,26 @@
+import bcrypt from "bcrypt";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+import users from "./users.json" assert { type: "json" };
+import tunes from "./tunes.json" assert { type: "json" };
+import { User, Tune } from "../models/index.js";
+
+const SALT_ROUNDS = 10;
+
+export default async function seed() {
+	mongoose.set("strictQuery", false); // The default in Mongoose 7
+	await mongoose.connect(process.env.CONNECTION_STRING);
+
+	await User.deleteMany({});
+	await Tune.deleteMany({});
+
+	for (let user of users) {
+		user.password = await bcrypt.hash(user.password, SALT_ROUNDS);
+	}
+
+	await User.insertMany(users);
+	await Tune.insertMany(tunes);
+
+	await mongoose.disconnect();
+}
