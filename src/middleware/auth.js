5528bbe4f1ed98145ce3eb7e04e3a0cdd1697296
Teddy Putus <99917582+TeddyPutus@users.noreply.github.com>
2023-02-22 11:12:56 +0000

    implemented permission level middleware

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,4 +1,26 @@
-// Function to check permission level
-async function checkPermissionLevel(req, res, next, requiredPermissionLevel=0){
+import Role from "../constants/roles.json" assert { type: "json" };
+import User from "../models/User.model";
 
+// Specify required permission level for a route, set to User level (0) as default.
+export function permissionLevel(requiredPermissionLevel=Role.USER){
+    // Function to check permission level.
+    return async function checkPermissionLevel(req, res, next){
+        // Extract the User from the request (attached to req by token verification middleware).
+        const foundUser = req.user;
+        
+        if(foundUser){
+            // If the permission level is OK, go to next function.
+            if(foundUser.role >= requiredPermissionLevel){
+                next();
+            }else{
+                // Else, send 401 status (Unauthorised).
+                res.sendStatus(401);
+            }
+        }else{
+            // User not found.
+            res.sendStatus(404);
+        }
+    }
 }
+
+
