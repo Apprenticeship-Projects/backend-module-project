a676ff100b62bd77e9aeedda12fd6dacd78461a7
George Reeve <george.fr.reeve@gmail.com>
2023-02-22 11:50:19 +0000

    Run seed on setup

--- tests/setup.js
+++ tests/setup.js
@@ -1,7 +1,9 @@
 import dotenv from "dotenv";
 import { connect } from "../src/utils/db.js";
+import seed from "../src/data/seedFn.js";
 
 export default async () => {
 	dotenv.config({ path: ".env.test" });
+	await seed();
 	await connect();
 };
