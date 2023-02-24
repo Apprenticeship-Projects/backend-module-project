7ffd75627f3f9a14d255be73cf3d1e51c681aa03
George Reeve <george.fr.reeve@gmail.com>
2023-02-24 15:20:02 +0000

    Add js extension

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { splitToken, verifyToken } from "../utils/token";
+import { splitToken, verifyToken } from "../utils/token.js";
 
 const router = Router();
 
