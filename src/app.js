098cc0c5c19fe59f9756af038ea4515e00dfd559
Leon Semmens <114012182+leonsemmens-sky@users.noreply.github.com>
2023-02-24 15:07:14 +0000

    Add cookie parser

--- src/app.js
+++ src/app.js
@@ -1,15 +1,22 @@
-import express from 'express';
-import cors from 'cors';
-import {userRouter, tuneRouter, sessionRouter, registerRouter} from './routes/index.js';
+import express from "express";
+import cors from "cors";
+import {
+	userRouter,
+	tuneRouter,
+	sessionRouter,
+	registerRouter,
+} from "./routes/index.js";
+import cookieParser from "cookie-parser";
 
 const app = express(); //create a new instance of express
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors()); //This allows post requests etc.
+app.use(cookieParser());
 
-app.use('/user', userRouter);
-app.use('/tune', tuneRouter);
-app.use('/session', sessionRouter);
-app.use('/register', registerRouter);
+app.use("/user", userRouter);
+app.use("/tune", tuneRouter);
+app.use("/session", sessionRouter);
+app.use("/register", registerRouter);
 
-export {app};
+export { app };
