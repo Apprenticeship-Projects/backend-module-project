34436803ef0ba9434ffd5f2013fb3182eb8b4cee
Ana Moros <ana.morosmarmol@sky.uk>
2023-03-03 12:12:27 +0000

    added GET/PUT/DELETE

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -3,120 +3,113 @@ import { body } from "express-validator";
 import { auth, permissionLevel } from "../middleware/auth.js";
 import Role from "../constants/roles.json" assert { type: "json" };
 import { User } from "../models/index.js";
-import { createHash } from "../utils/hash.js";
 import { toUTCDate } from "../utils/utc.js";
 
 const router = Router();
 
-// // Routes for /user
-// router.get("/", auth, permissionLevel(Role.USER), (req, res) => {
-//   if (req.user) {
-//     const { username, email, firstName, lastName, dob, ratings, tunes } =
-//       req.user;
-//     res.status(200).send({
-//       username,
-//       email,
-//       firstName,
-//       lastName,
-//       dob,
-//       ratings: ratings.length,
-//       tunes: tunes.length,
-//     });
-//   } else {
-//     res.status(401).send("not authorized");
-//   }
-// });
+router.use(auth, permissionLevel(Role.ADMIN));
 
-// router.put(
-//   "/",
-//   body("newEmail").optional().isEmail().isLength({ min: 2, max: 32 }),
-//   body("password").optional().isLength({ min: 8, max: 20 }).not().contains(" "),
-//   body("newPassword")
-//     .optional()
-//     .isLength({ min: 8, max: 20 })
-//     .not()
-//     .contains(" "),
-//   body("newUsername")
-//     .optional()
-//     .isLength({ min: 5 })
-//     .not()
-//     .matches(/[^a-zA-Z0-9_]/),
-//   body("newFirstName").optional().isAlpha().isLength({ min: 1 }),
-//   body("newLastName").optional().isAlpha().isLength({ min: 1 }),
-//   body("newDob")
-//     .optional()
-//     .matches(/\d\d\d\d\/\d\d\/\d\d/),
-//   auth,
-//   permissionLevel(Role.USER),
-//   async (req, res) => {
-//     if (req.user) {
-//       //if auth has been passed, user details will be attached to the request
-//       const { username, email, firstName, lastName, dob } = req.user;
+// Routes for /admin
+router.get("/users", (req, res) => {
+  if (req.user) {
+    try {
+      const allUsers = User.find().exec();
+      res.status(200).send(allUsers);
+    } catch (error) {
+      res.send(404).send(error);
+    }
+  } else {
+    res.status(401).send("not authorized");
+  }
+});
 
-//       const foundUser = await User.findOne({ _id: req.uid }).exec(); //find the user to update
+router.get("/users/:id", (req, res) => {
+  if (req.user) {
+    try {
+      const user = User.findOne({ _id: req.query.id }).exec();
+      res.status(200).send(user);
+    } catch (error) {
+      res.send(404).send(error);
+    }
+  } else {
+    res.status(401).send("not authorized");
+  }
+});
 
-//       if (!foundUser) {
-//         res.status(404).send("User not found");
-//         return;
-//       }
+router.put(
+  "/users/:id",
+  body("newEmail").optional().isEmail().isLength({ min: 2, max: 32 }),
+  body("newUsername")
+    .optional()
+    .isLength({ min: 5 })
+    .not()
+    .matches(/[^a-zA-Z0-9_]/),
+  body("newFirstName").optional().isAlpha().isLength({ min: 1 }),
+  body("newLastName").optional().isAlpha().isLength({ min: 1 }),
+  body("newDob")
+    .optional()
+    .matches(/\d\d\d\d\/\d\d\/\d\d/),
+  async (req, res) => {
+    if (req.user) {
+      try {
+        const foundUser = await User.findOne({ _id: req.query.id }).exec(); //find the user to update
 
-//       // All update values are optional in the body, only update if they exist
-//       foundUser.username = req.body.newUsername
-//         ? req.body.newUsername
-//         : username;
-//       foundUser.email = req.body.newEmail ? req.body.newEmail : email;
-//       foundUser.firstName = req.body.newFirstName
-//         ? req.body.newFirstName
-//         : firstName;
-//       foundUser.lastName = req.body.newLastName
-//         ? req.body.newLastName
-//         : lastName;
-//       foundUser.dob = req.body.newDob
-//         ? toUTCDate(new Date(req.body.newDob))
-//         : dob;
+        if (!foundUser) {
+          res.status(404).send("User not found");
+          return;
+        }
 
-//       // Special case: Only update password if user has given their old password
-//       if (req.body.newPassword) {
-//         if (!req.body.password) {
-//           res.status(401).send("not authorized");
-//           return;
-//         } else {
-//           const hashedPass = await createHash(req.body.password);
+        // All update values are optional in the body, only update if they exist
+        foundUser.username = req.body.newUsername
+          ? req.body.newUsername
+          : foundUser.username;
+        foundUser.email = req.body.newEmail
+          ? req.body.newEmail
+          : foundUser.email;
+        foundUser.firstName = req.body.newFirstName
+          ? req.body.newFirstName
+          : foundUser.firstName;
+        foundUser.lastName = req.body.newLastName
+          ? req.body.newLastName
+          : foundUser.lastName;
+        foundUser.dob = req.body.newDob
+          ? toUTCDate(new Date(req.body.newDob))
+          : dob;
 
-//           if (foundUser.password !== hashedPass) {
-//             res.status(401).send("not authorized");
-//             return;
-//           }
+        // Save the updated user and return the new object
+        await foundUser.save();
 
-//           foundUser.password = hashedPass;
-//         }
-//       }
+        res.status(200).send({
+          username: foundUser.username,
+          email: foundUser.email,
+          firstName: foundUser.firstName,
+          lastName: foundUser.lastName,
+          dob: foundUser.dob,
+          ratings: foundUser.ratings.length,
+          tunes: foundUser.tunes.length,
+        });
 
-//       // Save the updated user and return the new object
-//       await foundUser.save();
+        res.status(200).send(user);
+      } catch (error) {
+        res.send(404).send(error);
+      }
+    } else {
+      res.status(401).send("not authorized");
+    }
+  }
+);
 
-//       res.status(200).send({
-//         username: foundUser.username,
-//         email: foundUser.email,
-//         firstName: foundUser.firstName,
-//         lastName: foundUser.lastName,
-//         dob: foundUser.dob,
-//         ratings: foundUser.ratings.length,
-//         tunes: foundUser.tunes.length,
-//       });
-//     } else {
-//       res.status(401).send("not authorized");
-//     }
-//   }
-// );
-
-// router.delete("/", auth, permissionLevel(Role.USER), async (req, res) => {
-//   if (req.user) {
-//     const dbResult = await User.deleteOne({ _id: req.uid }).exec();
-//     res.status(202).send("deleted");
-//   } else {
-//     res.status(401).send("not authorized");
-//   }
-// });
+router.delete("/users/:id", async (req, res) => {
+  if (req.user) {
+    try {
+      await User.deleteOne({ _id: req.query.id }).exec();
+      res.status(202).send("deleted");
+    } catch (error) {
+      res.send(404).send(error);
+    }
+  } else {
+    res.status(401).send("not authorized");
+  }
+});
 
 export { router };
